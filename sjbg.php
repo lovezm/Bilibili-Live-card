1bcba88bbc589f4d2846f29ecf222898273d7d9a
WangMuDu <1245240736@qq.com>
2017-05-03 20:50:57 +0800

    Create sjbg.php
    随机背景

---
+++ sjbg.php
@@ -0,0 +1,28 @@
+<?php
+$sjbg =array(
+"1" => "https://cdn.9yo.cc/sjbg1.jpg",
+"2" => "https://cdn.9yo.cc/sjbg2.jpg",
+"3" => "https://cdn.9yo.cc/sjbg3.jpg",
+"4" => "https://cdn.9yo.cc/sjbg4.jpg",
+"5" => "https://cdn.9yo.cc/sjbg5.jpg",
+"6" => "https://cdn.9yo.cc/sjbg6.jpg",
+"7" => "https://cdn.9yo.cc/sjbg7.jpg",
+"8" => "https://cdn.9yo.cc/sjbg8.jpg",
+"9" => "https://cdn.9yo.cc/sjbg9.jpg",
+"10" => "https://cdn.9yo.cc/sjbg10.jpg",
+"11" => "https://cdn.9yo.cc/sjbg11.jpg",
+"12" => "https://cdn.9yo.cc/sjbg12.jpg",
+"13" => "https://cdn.9yo.cc/sjbg13.jpg",
+"14" => "https://cdn.9yo.cc/sjbg14.jpg",
+"15" => "https://cdn.9yo.cc/sjbg15.jpg",
+"16" => "https://cdn.9yo.cc/sjbg16.jpg",
+"17" => "https://cdn.9yo.cc/sjbg17.jpg",
+"18" => "https://cdn.9yo.cc/sjbg18.jpg",
+"19" => "https://cdn.9yo.cc/sjbg19.jpg",
+"20" => "https://cdn.9yo.cc/sjbg20.jpg",
+"21" => "https://cdn.9yo.cc/sjbg21.jpg",
+"22" => "https://cdn.9yo.cc/sjbg22.jpg",
+"23" => "https://cdn.9yo.cc/sjbg23.jpg",
+);
+$r =rand(1,count($sjbg));
+?>
