38452d4f036ec792493849831229319ddc5118fc
WangMuDu <1245240736@qq.com>
2017-05-03 20:50:01 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,165 @@
+<html>
+
+<head>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Bilibili直播间简介卡生成工具,B站简介生成,txsb,txsb.vip">
+    <meta name="keywords" content="Bilibili直播间简介卡生成工具,B站简介生成,txsb,txsb.vip">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Bilibili直播间简介卡生成工具-王慕独,www.txsb.vip</title>
+ <meta name="renderer" content="webkit">
+  <meta http-equiv="Cache-Control" content="no-siteapp" />
+  <link rel="stylesheet" href="font/style.css?v=20170420">
+    <link rel="stylesheet" href="css/lq.lib.css?v=20170420">
+    <link rel="stylesheet" href="css/lqmoeui.css?v=20170420">
+
+</HEAD>
+<body>
+  <div class="container">
+<h1 class="text-center">Bilibili直播间简介卡生成工具-王慕独</h1>
+<nav class="navbar bg-dark mb">
+          <a class="navbar-toggle show@s"><i class="wt-three-line"></i></a>
+          <div class="navbar-content">
+            <ul class="nav hide@s">
+              <li class="hide-sm"><a href="javascript:;" class="navbar-logo">TXSB</a></li>
+              <li class="on"><a href="javascript:;">Home</a></li>
+              <li><a href="faq.html" target="_blank">FAQ</a></li>
+              <li><a href="young.html" target="_blank">Style</a></li>
+              <li><a href="htts://blog.233.re" target="_blank">Blog</a></li>
+			  <li><a href="http://weibo.com/wangmudu" target="_blank">Weibo</a></li>
+			  <li><a href="http://wpa.qq.com/msgrd?v=3&uin=55066344&site=qq&menu=yes" target="_blank">Q-me</a></li>
+			  <li><a href="v.php" target="_blank">Video</a></li>
+              <li><a href="https://www.233.re" target="_blank">图片连接获取</a></li>
+            </ul>
+          </div>
+        </nav>
+<section id="Codes">
+        
+        <pre data-lang="HTML"><code>支持HTML代码的换行符/A标签连接符(用于加群连接)/等 如果你不会用 请看提交按钮下面的食用说明!图片可以上传到www.233.re然后复制图片外链 具体请百度</code></pre>
+      </section>
+
+
+	<form action="" method="post" id="myform" name="myform" class="form">
+	
+<div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="bgimg"></textarea>
+              <span class="floating-label">背景图连接</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+<div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="alipay" ></textarea>
+              <span class="floating-label">支付宝二维码</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		 <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="jianjie"></textarea>
+              <span class="floating-label">主播简介(非必填)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		  <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="neirong"></textarea>
+              <span class="floating-label">直播内容(非必填)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		   <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="peizhi"></textarea>
+              <span class="floating-label">电脑配置/直播时间(非必填)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		 <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="lianxi"></textarea>
+              <span class="floating-label">勾搭方式/联系方式(非必填)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		 <h6 class="text-center">以下内容非必填项目 只有在有这个功能的简介卡需要填写 都有详细介绍</h6>
+		  <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="name"></textarea>
+              <span class="floating-label">主人名字(建议两个字)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+		   <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="touxiang"></textarea>
+              <span class="floating-label">主人头像连接</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+ <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="haoma"></textarea>
+              <span class="floating-label">直播间号码(有切换功能的需要填直播间号码如110 如无切换功能 不用填)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+ <div class="mb">
+            <div class="control-eff">
+              <textarea rows="1" name="weibo"></textarea>
+              <span class="floating-label">微博地址(有微博功能的简介卡需要填写 完整连接 如http://weibo.com/wangmudu)</span>
+              <span class="input-bar"></span>
+            </div>
+          </div>
+<script>
+function yangshi1(temp){
+document.getElementById("myform").action="post.php";
+}
+function yangshi2(temp){
+document.getElementById("myform").action="post2.php";
+}
+function yangshi3(temp){
+document.getElementById("myform").action="post3.php";
+}
+function yangshi4(temp){
+document.getElementById("myform").action="post4.php";
+}
+function yangshi5(temp){
+document.getElementById("myform").action="post5.php";
+}
+</script>
+<div class="mb">
+开源版本仅提供一个样式 方便开发（请选择样式E）
+            <label class="label">选择样式(<a href="young.html" target="_blank">点我查看所有样式</a>)图片多,流量党不要点(Look是单独查看样式)</label>
+            <div class="control">
+              <label class="radio"><input type="radio" name="radio1" onclick="yangshi1()"><i class="icon"></i> 样式A<a href="http://cdn.9yo.cc/a.png" target="_blank">Look</a></label>
+              <label class="radio"><input type="radio" name="radio1" onclick="yangshi2()"><i class="icon"></i> 样式B<a href="http://cdn.9yo.cc/b.png" target="_blank">Look</a></label>
+			  <label class="radio"><input type="radio" name="radio1" onclick="yangshi3()"><i class="icon"></i> 样式C(必填房间号)<a href="http://cdn.9yo.cc/c.png" target="_blank">Look</a></label>
+			  <label class="radio"><input type="radio" name="radio1" onclick="yangshi4()"><i class="icon"></i> 样式D(必填头像)<a href="http://cdn.9yo.cc/d.png" target="_blank">Look</a></label>
+			  <label class="radio"><input type="radio" name="radio1" onclick="yangshi5()"><i class="icon"></i> 样式E<a href="http://cdn.9yo.cc/e.png" target="_blank">Look</a></label>
+            </div>
+          </div>
+
+<input class="width btn bg-primary" type="submit" value="生成"/>
+<ol class="breadcrumb">
+          <li>生成以后 直接右键查看页面源码 然后全选复制 到直播间修改信息 简介换成代码模式 粘贴 不要问我为啥要这样 因为我不会把它显示出来啊</li>
+          <li><a href="http://www.bilibili.com/video/av9949180/" target="_blank">使用视频教程</a></li>
+          <li><a href="faq.html" target="_blank">第一次用点此</a></li>
+          
+        </ol>
+
+</form>
+
+<center><img src="http://cdn.9yo.cc/a.png" width="1200" height="300"></center><br>
+<blockquote>
+          <p>目前正在测试阶段 估计一天只会出一个新的样式 如果你愿意可以加入我 联系我请在微博@王慕独</p>
+          <footer>随笔 @王慕独</footer>
+        </blockquote>
+		
+		<div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"2","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+		
+<h6 class="text-center">开源版本不提供前端框架</h6>
+</div>
+</body>
+</html>
